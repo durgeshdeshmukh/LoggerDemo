47f6108b6475f0eb544d03fa36caf6188b6469c0
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-17 23:27:45 -0800

    Add project files.

---
+++ LoggerDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LoggerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers obj = new AddNumbers();
+            obj.Sum(2, 3);
+            Console.WriteLine("This is a Logger Demo");
+            Console.ReadKey();
+        }
+    }
+}
